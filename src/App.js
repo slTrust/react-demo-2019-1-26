5d8714c0e0a44837698af8a26f97581ee8efa18c
huangjiaxi <trustfor@sina.cn>
2019-01-26 16:32:21 +0800

    第一次提交app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function sayHi(){
+  alert('hi')
+}
+
 class App extends Component {
   render() {
     return (
@@ -9,16 +13,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            React也不过如此吗？
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={sayHi}>点我</button>
         </header>
       </div>
     );
